99b64bf0d602e9aa1da171b629b0354a1feb5f06
Seung Min Lee <semileko@gmail.com>
2022-08-19 04:05:29 -0400

    Created a SelectField component that is a dropdown selection component.

---
+++ ui/src/components/Form/SelectField.js
@@ -0,0 +1,39 @@
+import { getIn } from 'formik'
+import MuiSelectField from '@material-ui/core/Select'
+import React from 'react'
+
+const fieldToSelectField = ({
+  disabled,
+  field: { onBlur: fieldOnBlur, ...field },
+  form: { errors, isSubmitting, touched },
+  helperText,
+  onBlur,
+  required,
+  variant,
+  warning,
+  ...props
+}) => {
+  const dirty = getIn(touched, field.name)
+  const fieldError = getIn(errors, field.name)
+  const showError = dirty && !!fieldError
+  return {
+    variant: variant,
+    error: showError,
+    helperText: showError ? fieldError : warning ?? helperText,
+    disabled: disabled ?? isSubmitting,
+    onBlur: (event) => onBlur ?? fieldOnBlur(event ?? field.name),
+    ...field,
+    ...props,
+  }
+}
+
+export const SelectField = ({ children, ...props }) =>
+  <MuiSelectField {...fieldToSelectField(props)}>
+    {children}
+  </MuiSelectField>
+
+
+export default SelectField
+
+SelectField.displayName = 'FormikSelectField'
+SelectField.tabIndex = 0
